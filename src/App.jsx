06e6589f332fdd42e9de6fba85145a4494514adc
Sarang19 <rastogi.sarang2004@gmail.com>
2025-06-28 13:38:12 +0530

    initial commit: Added Navbar, HeroSection improvised loading screen

---
+++ src/App.jsx
@@ -0,0 +1,51 @@
+import gsap from 'gsap';
+import { ScrollTrigger } from 'gsap/all';
+import React, { useState, useEffect } from 'react';
+
+import NavBar from './sections/NavBar';
+import Hero from './sections/Hero';
+
+function App() {
+  const [loading, setLoading] = useState(true);
+  const [showHero, setShowHero] = useState(false);
+
+  useEffect(() => {
+    gsap.registerPlugin(ScrollTrigger);
+    const timer = setTimeout(() => {
+      setLoading(false);
+      setShowHero(true);
+    }, 5000);
+    return () => clearTimeout(timer);
+  }, []);
+
+  if (loading) {
+    return (
+      <div style={{
+        width: '100vw',
+        height: '100vh',
+        overflow: 'hidden',
+        background: '#18181b',
+      }}>
+        <img
+          src="/images/rockstar.gif"
+          alt="Rockstar Loading"
+          style={{
+            width: '100vw',
+            height: '100vh',
+            objectFit: 'cover',
+            display: 'block',
+          }}
+        />
+      </div>
+    );
+  }
+
+  return (
+    <>
+    <NavBar />
+    <Hero show={showHero} />
+    </>
+  )
+}
+
+export default App
